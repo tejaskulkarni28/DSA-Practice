4e2a94a7ce5dae96e31f381dfb92356052eb83fb
Tejas Kulkarni <test@gmail.com>
2022-01-12 19:17:53 +0530

    Updated Insertion and Selection Sort Program.

---
+++ Insertion Sort/src/com/company/Main.java
@@ -0,0 +1,27 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int[] nums = new int[] {9,8,7};
+        InsertionSort(nums);
+        for(int i=0; i<nums.length; i++){
+            System.out.println(nums[i]);
+        }
+    }
+    static void InsertionSort(int[] nums){
+        for(int i=0; i<nums.length -1; i++){
+            for(int j=i+1; j > 0; j--){
+                if(nums[j] < nums[j-1]){
+                    int temp = nums[j];
+                    nums[j] = nums[j-1];
+                    nums[j-1] = temp;
+                }
+                else{
+                    break;
+                }
+            }
+        }
+    }
+}
--- SelectionSort/src/com/company/Main.java
+++ SelectionSort/src/com/company/Main.java
@@ -5,8 +5,37 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         // Selection Sort
+        int[] nums = new int[]{9,8,10,6};
+        SelectionSort(nums);
+        for(int i=0; i<nums.length; i++){
+            System.out.println(nums[i]);
+        }
     }
     static void SelectionSort(int[] nums){
-        
+        for (int i = 0; i < nums.length; i++) {
+            // find the max item in the remaining array and swap with correct index
+            int last = nums.length - i - 1;
+            int maxIndex = getMaxIndex(nums, 0, last);
+
+            swap(nums, maxIndex, last);
+        }
+    }
+    static void swap(int[] nums, int first, int second){
+        int temp = nums[first];
+        nums[first] = nums[second];
+        nums[second] = temp;
+    }
+
+    static int getMaxIndex(int[] nums, int start, int end){
+
+        int max = start;
+
+        for (int i = 0; i <= end ; i++) {
+            if(nums[max] < nums[i]){
+                max = i;
+            }
+        }
+
+        return max;
     }
 }
